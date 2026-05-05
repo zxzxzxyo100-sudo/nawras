674245ec78a230aae41e07acd2d95bfa4aaaf1c4
zxzxzxyo100-sudo <zxzxzxyo100@gmail.com>
2026-05-05 16:10:45 +0200

    Update database password for staging and production

--- api-php/config.php
+++ api-php/config.php
@@ -33,13 +33,13 @@ if ($_isStaging) {
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'u495355717_nawras_stg');
     define('DB_USER', 'u495355717_nawras_stg');
-    define('DB_PASS', 'Zidona11@');
+    define('DB_PASS', 'Alizidona2');
     define('IS_STAGING_ENV', true);
 } else {
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'u495355717_nawras_crm');
     define('DB_USER', 'u495355717_nawras_admin');
-    define('DB_PASS', 'Zidona11');
+    define('DB_PASS', 'Alizidona2');
     define('IS_STAGING_ENV', false);
 }
 
